1260737c89fb222c73d3b8ca796dd3ca8d54e311
Markus Lenger <markus.lenger@gmail.com>
2022-11-27 19:24:26 +0100

    initial implementation working

---
+++ src/lib/drop-intermediate.spec.ts
@@ -0,0 +1,123 @@
+import { Subject } from 'rxjs';
+import { tap, toArray } from 'rxjs/operators';
+
+import { dropIntermediate } from './drop-intermediate';
+
+describe('dopIntermediate', () => {
+  it('should drop intermediate values', async () => {
+    const subject = new Subject<number>();
+    const actualPromise = subject
+      .pipe(dropIntermediate(), toArray())
+      .toPromise();
+
+    for (let i = 0; i < 3; i++) {
+      setTimeout(() => subject.next(i));
+    }
+    setTimeout(() => subject.complete());
+    const actual = await actualPromise;
+
+    expect(actual).toEqual([2]);
+  });
+
+  it('should deliver all values if there is enough time', async () => {
+    const subject = new Subject<number>();
+    const actualPromise = subject
+      .pipe(
+        dropIntermediate(),
+        tap((v) => {
+          if (v < 2) {
+            setTimeout(() => subject.next(v + 1), 0);
+          } else {
+            subject.complete();
+          }
+        }),
+        toArray()
+      )
+      .toPromise();
+
+    setTimeout(() => subject.next(0));
+    const actual = await actualPromise;
+
+    expect(actual).toEqual([0, 1, 2]);
+  });
+
+  it('should allow recursion', async () => {
+    const subject = new Subject<number>();
+    const actualPromise = subject
+      .pipe(
+        dropIntermediate(),
+        tap((v) => {
+          if (v < 2) {
+            subject.next(v + 1);
+          } else {
+            subject.complete();
+          }
+        }),
+        toArray()
+      )
+      .toPromise();
+
+    setTimeout(() => subject.next(0));
+    const actual = await actualPromise;
+
+    expect(actual).toEqual([0, 1, 2]);
+  });
+
+  it('should forward errors correctly', async () => {
+    let actual: unknown | undefined;
+    const subject = new Subject<number>();
+    const error = new Error('Test');
+    const actualPromise = subject
+      .pipe(
+        dropIntermediate(),
+        tap((v) => {
+          if (v < 2) {
+            setTimeout(() => subject.next(v + 1), 0);
+          } else {
+            subject.error(error);
+          }
+        })
+      )
+      .toPromise();
+
+    setTimeout(() => subject.next(0));
+    try {
+      await actualPromise;
+    } catch (e) {
+      actual = e;
+    }
+
+    expect(actual).toBe(error);
+  });
+
+  it('should forward values before error correctly', async () => {
+    const actual = [];
+    const subject = new Subject<number>();
+    const error = new Error('Test');
+    const actualPromise = subject
+      .pipe(
+        dropIntermediate(),
+        tap((v) => {
+          actual.push(v);
+          if (v < 1) {
+            setTimeout(() => subject.next(v + 1), 0);
+          } else {
+            setTimeout(() => {
+              subject.next(2);
+              subject.error(error);
+            }, 0);
+          }
+        })
+      )
+      .toPromise();
+
+    setTimeout(() => subject.next(0));
+    try {
+      await actualPromise;
+    } catch (e) {
+      // ignore
+    }
+
+    expect(actual).toEqual([0, 1, 2]);
+  });
+});
---
+++ src/lib/drop-intermediate.ts
@@ -0,0 +1,52 @@
+import { Observable } from 'rxjs';
+
+export function dropIntermediate() {
+  return function <T>(source: Observable<T>): Observable<T> {
+    return new Observable((subscriber) => {
+      let updateEvent: number | undefined;
+      let nextValue: T | undefined;
+      let complete = false;
+      let error: unknown;
+
+      function scheduleUpdate(value: T): number {
+        return setTimeout(() => {
+          let currentValue = nextValue ?? value;
+          while (currentValue !== undefined) {
+            nextValue = undefined;
+            subscriber.next(currentValue);
+            currentValue = nextValue;
+          }
+          if (complete) {
+            subscriber.complete();
+          }
+          if (error) {
+            subscriber.error(error);
+          }
+          updateEvent = undefined;
+        }, 0);
+      }
+
+      return source.subscribe({
+        next(value) {
+          if (updateEvent === undefined) {
+            updateEvent = scheduleUpdate(value);
+          } else {
+            nextValue = value;
+          }
+        },
+        error(e) {
+          error = e;
+          if (updateEvent === undefined) {
+            subscriber.error(e);
+          }
+        },
+        complete() {
+          complete = true;
+          if (updateEvent === undefined) {
+            subscriber.complete();
+          }
+        },
+      });
+    });
+  };
+}
